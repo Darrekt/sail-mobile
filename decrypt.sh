af70097f8e6fc1bd21af21a6864be15314de210b
Darrekt <jkricklau@gmail.com>
2021-08-06 19:20:57 +0800

    Add decryption for firebase configs

---
+++ decrypt.sh
@@ -0,0 +1,7 @@
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_DECRYPTION_SECRET" --output firebase_configs.tar.gz firebase_configs.tar.gz.gpg 
+tar xvf firebase_configs.tar.gz
+rm firebase_configs.tar.gz
